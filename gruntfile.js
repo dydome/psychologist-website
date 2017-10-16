d7a32ffb495d45d47d1e9ee0d241b38bf3495a47
Michal Dydo <michal1.dydo@gmail.com>
2017-10-16 14:12:42 +0200

    First push

---
+++ gruntfile.js
@@ -0,0 +1,85 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'css/style.css': 'css/style.scss'
+                }
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['css/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false,
+                },
+            }
+        },
+
+        cssmin: {
+            options: {
+                mergeIntoShorthands: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'css',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'css',
+                    ext: '.min.css'
+                }]
+            }
+        },
+
+        uglify: {
+            my_target: {
+                files: {
+                    'js/app.min.js': 'js/app.js'
+                }
+            }
+        },
+
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: ['**/*.{png,jpg,gif,svg}'],
+                    dest: 'compresed/'
+                }]
+            }
+        },
+
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true,
+                    minifyCSS: true,
+                    minifyJS: true
+                },
+                files: {
+                    'dist/index.html': 'index.html', // 'destination': 'source'
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+    // Default task(s)
+    grunt.registerTask('default', ['watch']);
+};
