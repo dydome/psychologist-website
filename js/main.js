a0dc1e74d34a506246f72f83d6d11fcb0733427a
Michal Dydo <michal1.dydo@gmail.com>
2017-10-17 19:54:44 +0200

    ADD dalsze poprawki

--- js/main.js
+++ js/main.js
@@ -1,5 +1,52 @@
 $(document).ready(function () {
 
-
-
 });
+
+function openChronologyTab(index) {
+    hideText();
+    showButtons()
+    switch (index) {
+        case 1:
+            $(".description-first").show();
+            $(".button-first").hide();
+            break;
+        case 2:
+            $(".description-second").show();
+            $(".button-second").hide();
+            break;
+        case 3:
+            $(".description-third").show();
+            $(".button-third").hide();
+            break;
+        case 4:
+            $(".description-fourth").show();
+            $(".button-fourth").hide();
+            break;
+        case 5:
+            $(".description-fifth").show();
+            $(".button-fifth").hide();
+            break;
+        case 6:
+            $(".description-sixth").show();
+            $(".button-sixth").hide();
+            break;
+    }
+}
+function hideText() {
+    $(".description-first").hide();
+    $(".description-second").hide();
+    $(".description-third").hide();
+    $(".description-fourth").hide();
+    $(".description-fifth").hide();
+    $(".description-sixth").hide();
+}
+
+function showButtons() {
+    $(".button-first").show();
+    $(".button-second").show();
+    $(".button-third").show();
+    $(".button-fourth").show();
+    $(".button-fifth").show();
+    $(".button-sixth").show();
+}
+
